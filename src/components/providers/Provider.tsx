b6f1fb747456216a045b2a20b4f91cb8339aa7fd
iMADi-ARCH <nandanaditya985@gmail.com>
2024-04-30 23:57:58 +0530

    refactor: load features from module async

--- src/components/providers/Provider.tsx
+++ src/components/providers/Provider.tsx
@@ -3,10 +3,13 @@ import { BaseProps } from "@/lib/types";
 import { LazyMotion, MotionConfig, domAnimation } from "framer-motion";
 import React from "react";
 
+const loadFeatures = () =>
+  import("./framer-features").then((res) => res.default);
+
 export const Providers = ({ children }: BaseProps) => {
   return (
     <>
-      <LazyMotion features={domAnimation}>
+      <LazyMotion strict features={loadFeatures}>
         <MotionConfig transition={{}}>{children}</MotionConfig>
       </LazyMotion>
     </>

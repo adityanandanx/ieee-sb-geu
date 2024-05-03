5b1a7bcded04773e67691af402fe5cff800a109f
iMADi-ARCH <nandanaditya985@gmail.com>
2024-05-03 11:09:39 +0530

    feat: browse events button

--- src/app/(home)/_sections/Events.tsx
+++ src/app/(home)/_sections/Events.tsx
@@ -1,7 +1,8 @@
 import { EventCard } from "@/app/events/_components/event-cards/EventCard";
 import { getEvents } from "@/app/events/actions";
+import { Button } from "@/components/ui/button";
 import { Heading } from "@/components/ui/typography";
-import React from "react";
+import Link from "next/link";
 
 type Props = {};
 
@@ -9,7 +10,7 @@ export const Events = async (props: Props) => {
   const events = await getEvents(3);
   return (
     <section className="relative h-svh max-h-[1024px] flex flex-col justify-center">
-      <div className="container py-10 gap-5">
+      <div className="container py-10 gap-5 flex flex-col items-start">
         <Heading>Recent Events</Heading>
         {events.length === 0 && <p>Events Coming Soon</p>}
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
@@ -17,6 +18,9 @@ export const Events = async (props: Props) => {
             <EventCard event={event} key={event.id} />
           ))}
         </div>
+        <Link href={"/events"}>
+          <Button size="lg">Browse All Events</Button>
+        </Link>
       </div>
     </section>
   );

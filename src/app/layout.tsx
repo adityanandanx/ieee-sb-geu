849a4ab4f96fdbc5b8ad085259ea3117ffe8972d
iMADi-ARCH <nandanaditya985@gmail.com>
2024-05-03 11:04:49 +0530

    fix: metaDatabase warning

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,10 +17,17 @@ const Providers = dynamic(
   }
 );
 
+const vercelUrl = process.env.VERCEL_URL;
+
 export const metadata: Metadata = {
   title: "IEEE Student Branch - Graphic Era University",
   description:
     "The IEEE GEU student branch provides a platform for members to develop their technical skills and become future-ready talents in the field of engineering & academic research. It offer events, workshops, and various opportunities to stay updated with industry trends and gain valuable insights.",
+  metadataBase: new URL(
+    vercelUrl
+      ? `https://${vercelUrl}`
+      : `http://localhost:${process.env.PORT || 3000}`
+  ),
 };
 
 export default function RootLayout({
